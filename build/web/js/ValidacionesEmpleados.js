1ff5996828c4b914478ab6d8d78829d9c2af0529
jdn0215 <jdn0213@gmail.com>
2017-04-15 21:31:31 -0600

    revision de validaciones

--- build/web/js/ValidacionesEmpleados.js
+++ build/web/js/ValidacionesEmpleados.js
@@ -5,25 +5,25 @@
 const VALIDACIONES_EMPLEADOS=[
     new Validacion(
      "textNombre2"," -Verifique el nombre del empleado- ",
-     $id("textNombre2").value!=="" && $("#textNombre2").val().length<=30  
+     ()=> $id("textNombre2").value!=="" && $("#textNombre2").val().length<=30  
     ),
     new Validacion(
      "textCedula12"," -Numero de identificación no válida- ",
-     ()=>!$id("CF2").checked ? true:IDFORMAT.test(idCliente),
+     ()=>!$id("CF2").checked ? true:IDFORMAT.test(idEmpleado),
      "textCedula22","textCedula32"
     ),
 
     new Validacion(
      "MtextID2"," -Numero de identificación no válida- ",
-     ()=> $id("CF2").checked ? true : (idCliente.length!==0&&idCliente.length<31)
+     ()=> $id("CF2").checked ? true : (idEmpleado.length!==0&&idEmpleado.length<31)
     ),
     new Validacion(
      "textCelular2"," -Numero de celular no valido- ",
-     TELFORMAT.test($id("textCelular2").value) && $("#textCelular2").val().length<=30 
+     ()=>  TELFORMAT.test($id("textCelular2").value) && $("#textCelular2").val().length<=30 
     ),
     new Validacion(
      "textEmail2"," -Correo electronico no valido- ",
-     mailFormat.test($id("textEmail2").value) && $("#textEmail2").val().length<=30  
+     ()=>  mailFormat.test($id("textEmail2").value) && $("#textEmail2").val().length<=30  
     )
     
 ];
--- build/web/js/validacionesMotoEstado.js
+++ build/web/js/validacionesMotoEstado.js
@@ -3,7 +3,7 @@
 const VALIDACIONES_MOTO_ESTADO=[
     new Validacion(
       "textKm"," -Indique el kilometraje- ",
-        ($id("textKm").value!=="") && (!isNaN($id("textKm").value)) && ($id("textKm").value>=0)
+       ()=> ($id("textKm").value!=="") && (!isNaN($id("textKm").value)) && ($id("textKm").value>=0)
     )
     
 ];
