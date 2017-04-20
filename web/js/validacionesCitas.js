bf589d67b006bfb4e6fa2d053ddd5f0a1a176e82
cmadrigalq <cmadrigal.3192@gmail.com>
2017-04-19 19:48:23 -0600

    Cambio en validaciones citas

--- web/js/validacionesCitas.js
+++ web/js/validacionesCitas.js
@@ -9,77 +9,77 @@
 
 const VALIDACIONES_CITAS = [
     new Validacion(
-        "clienteCita","Cliente no válido  ",
+        "clienteCita","-Cliente no válido, verifique que el numero de identificacion sea el correcto-  ",
         ()=> modoUpdate || CLIENTEBUSCADO!==null   
     ),
     
     new Validacion(
-        "motocito","Seleccione una motocicleta  ",
+        "motocito"," -Seleccione una motocicleta-  ",
         ()=> modoUpdate ||CLIENTEBUSCADO!==null&&motosDelClienteBuscado.length!==0   
     ),
     
     new Validacion(
-        "recepcionistaCita","Recepcionista?  ",
+        "recepcionistaCita"," -El recepcionista no es valido-  ",
         ()=> $id("recepcionistaCita").value !== "" && $id("recepcionistaCita").value.length < 21  
     ),
     
     new Validacion(
-        "proformaCita","número de proforma  ",
+        "proformaCita"," -Número de proforma no valido- ",
         ()=> $id("proformaCita").value!=="" && !isNaN($id("proformaCita").value)  
     ),
     
-    new Validacion(
+    /*new Validacion(
         "tipoCita","Especifique el tipo de trabajo  ",
         ()=> $id("tipoCita").value !== ""
-    ),
+    ),*/
     
     new Validacion(
-        "numeroOrdenCita","número de orden  ",
+        "numeroOrdenCita","- Número de orden no valido-  ",
         ()=> $id("citaPendiente").checked?true:$id("numeroOrdenCita").value!=="" && !isNaN($id("numeroOrdenCita").value)  
     ),
     
     new Validacion(
-        "EmpleadoCita"," ingrese el mes de la cita " ,
+        "EmpleadoCita"," -Ingrese el empleado de la cita- " ,
         ()=>modoUpdate ? true : $id("EmpleadoCita").selectedIndex!==0
     ),
     
     new Validacion(
-        "_mesCitas"," ingrese el mes de la cita " ,
+        "_mesCitas"," -Ingrese el mes de la cita- " ,
         ()=>modoUpdate ? true : $id("_mesCitas").selectedIndex!==0
     ),
 
     new Validacion(
-        "_diaCitas"," - ingrese el día de la cita ",
+        "_diaCitas"," -Ingrese el día de la cita- ",
         ()=>modoUpdate ? true : $id("_diaCitas").selectedIndex!==0
     ),
     
     new Validacion(
-        "minCita"," - ingrese la hora de la cita ",
+        "minCita"," -Ingrese la hora de la cita- ",
         ()=>modoUpdate ? true : $id("minCita").value!==""
     ),
     
     new Validacion(
-        "_mesCitas2"," ingrese el mes prometido " ,
+        "_mesCitas2"," -Ingrese el mes prometido- " ,
         ()=>modoUpdate ? true : $id("_mesCitas2").selectedIndex!==0
     ),
 
     new Validacion(
-        "_diaCitas2"," - ingrese el día prometido ",
+        "_diaCitas2"," -Ingrese el día prometido- ",
         ()=>modoUpdate ? true : $id("_diaCitas2").selectedIndex!==0
     ),
     
     new Validacion(
-        "minCita2"," - ingrese la hora prometida ",
+        "minCita2"," -Ingrese la hora prometida- ",
         ()=>modoUpdate ? true : $id("minCita2").value!==""
     ),
     
     new Validacion(
-        "_mesCitas3"," ingrese el mes de la entrega " ,
+        "_mesCitas3"," -Ingrese el mes de la entrega- " ,
         ()=> !modoUpdate ? true : !$id("citaCumplida").checked?true:$id("_mesCitas3").selectedIndex!==0
     ),
     
     new Validacion(
-        "minCita4"," - ingrese la hora de la entrega ",
+        "minCita4"," -Ingrese la hora de la entrega- ",
         ()=> !modoUpdate ? true : !$id("citaCumplida").checked?true: $id("minCita4").value!==""
     )
 ];
